4c426298bd60b2a863ae3629ca8965c481b0c655
Sigolletes <juditreverter87@gmail.com>
2023-01-21 21:43:44 +0100

    Add more practice

--- fullStackOpen/notes/src/index.js
+++ fullStackOpen/notes/src/index.js
@@ -4,7 +4,7 @@ import './index.css'
 
 import axios from 'axios'
 
-axios.get('http://localhost:3001/notes').then(response => {
+axios.get('http://localhost:3001/api/notes').then(response => {
   const notes = response.data
   ReactDOM.createRoot(document.getElementById('root')).render(
     <App notes={notes} />
