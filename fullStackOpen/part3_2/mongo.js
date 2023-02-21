4c9ded7105b0f680daad51c39e1ff93240de7fcb
Sigolletes <webdevjudit@gmail.com>
2023-02-21 09:30:43 +0100

    uibuild

--- fullStackOpen/part3_2/mongo.js
+++ fullStackOpen/part3_2/mongo.js
@@ -1,4 +1,4 @@
-// PRACTICE APPLICATION - node mongo.js password
+/* // PRACTICE APPLICATION - node mongo.js password
 
 // LIBRARY MONGOOSE (high-level API) IS AN ODM (Object Document Mapper) for saving JS objects as Mongo documents
 const mongoose = require('mongoose')
@@ -84,4 +84,4 @@ Note.find({ important: true }).then(result => {
     console.log(note)
   })
   mongoose.connection.close()
-})
+}) */
